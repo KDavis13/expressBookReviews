e144379b04ac2207af97502851d8eba552cd9a56
Belia Ramos <ramos.torres.belia@gmail.com>
2025-10-04 19:25:21 +0200

    Implemented logic to authenticate

--- final_project/index.js
+++ final_project/index.js
@@ -12,6 +12,21 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+    const token = req.session.authorization;
+    if(token){
+        jwt.verify(token, "access", (err,user)=>{
+            if(!err){
+                req.user = user;
+                next();
+            }
+            else{
+                return res.status(403).json({message: "User not logged in"});
+            }
+        });
+    }
+    else{
+        return res.status(403).json({message: "User not logged in"});
+    }
 });
  
 const PORT =5000;
