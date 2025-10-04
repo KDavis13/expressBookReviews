211125175c7f964839739fd09e897092ccba6bd3
Belia Ramos <ramos.torres.belia@gmail.com>
2025-10-04 19:55:00 +0200

    Task 5: Implement endpoint to retrieve book reviews by ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -64,7 +64,13 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { isbn } = req.params;
+  const book = Object.values(books).find(b => b.isbn === isbn);
+  if (book) {
+    res.send(JSON.stringify(book.reviews, null, 4));
+  } else {
+    res.status(404).json({message: "Book not found"});
+  }
 });
 
 module.exports.general = public_users;
