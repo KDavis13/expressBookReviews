c3c8a8abfbd835f3fab8e5844750ed83ad6c170d
Belia Ramos <ramos.torres.belia@gmail.com>
2025-10-04 19:46:39 +0200

    Task 2: Add ISBN to book entries and implement endpoint to retrieve book details by ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,9 +28,15 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
+  const { isbn } = req.params;
+  const book = Object.values(books).find(b => b.isbn === isbn);
+  if (book) {
+    res.send(JSON.stringify(book, null, 4));
+  } else {
+    res.status(404).json({message: "Book not found"});
+  }
+});
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
