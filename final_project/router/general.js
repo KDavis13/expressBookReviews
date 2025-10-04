15bb2dc8f7dc170a06d1bfeb66ef1e4d5498528f
Belia Ramos <ramos.torres.belia@gmail.com>
2025-10-04 19:52:49 +0200

    Task 4: Implement endpoint to retrieve books by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -52,7 +52,13 @@ public_users.get('/author/:author',function (req, res) {
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { title } = req.params;
+  const booksByTitle = Object.values(books).filter(b => b.title === title);
+  if (booksByTitle.length > 0) {
+    res.send(JSON.stringify(booksByTitle, null, 4));
+  } else {
+    res.status(404).json({message: "No books found for this title"});
+  }
 });
 
 //  Get book review
